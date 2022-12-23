15ee3319741a6822017e3ace91db510a9fb41289
Jonathan Edwardsson <(none)>
2022-12-23 21:02:12 +0100

    Added gitignore and scroll offset

--- nvim/lua/webhooked/base.lua
+++ nvim/lua/webhooked/base.lua
@@ -23,6 +23,7 @@ vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.scrolloff = 999
 vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
